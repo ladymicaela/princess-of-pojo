f83a9fbb5bf2b1291f4a44644a862462a9ede1c6
Micaela Moss <mcmoss1991@gmail.com>
2020-01-28 17:09:35 -0800

    Add work from second tutorial walkthrough

--- src/index.js
+++ src/index.js
@@ -1,131 +1,437 @@
-console.log("Webpack is working!")
+class Vec {
+    constructor(x, y) {
+        this.x = x; this.y = y;
+    }
+    plus(other) {
+        return new Vec(this.x + other.x, this.y + other.y);
+    }
+    times(factor) {
+        return new Vec(this.x * factor, this.y * factor);
+    }
+}
 
+
+class Player {
+    constructor(pos, speed) {
+        this.pos = pos;
+        this.speed = speed;
+    }
+
+    get type() { return "player"; }
+
+    static create(pos) {
+        return new Player(pos.plus(new Vec(0, -0.5)),
+            new Vec(0, 0));
+    }
+}
+
+Player.prototype.size = new Vec(0.8, 1.5)
+
+const levelChars = {
+    ".": "empty",
+    "#": "wall",
+    "+": "lava",
+    "d": "door",
+    "@": Player
+};
+
+
+class Level {
+    constructor(plan) {
+        let rows = plan.trim().split("/").map(l => [...l]);
+        this.height = rows.length;
+        this.width = rows[0].length;
+        this.startActors = [];
+
+        this.rows = rows.map((row, y) => {
+            return row.map((ch, x) => {
+                let type = levelChars[ch];
+                if (typeof type === "string") return type;
+                this.startActors.push(
+                    type.create(new Vec(x, y), ch));
+                return "empty";
+            });
+        });
+    }
+}
+
+class State {
+    constructor(level, actors, status) {
+        this.level = level;
+        this.actors = actors;
+        this.status = status;
+    }
+
+    static start(level) {
+        return new State(level, level.startActors, "playing");
+    }
+
+    get player() {
+        return this.actors.find(a => a.type == "player");
+    }
+}
+
+
+function elt(name, attrs, ...children) {
+    let dom = document.createElement(name);
+    for (let attr of Object.keys(attrs)) {
+        dom.setAttribute(attr, attrs[attr]);
+    }
+    for (let child of children) {
+        dom.appendChild(child);
+    }
+    return dom;
+}
+
+
+class DOMDisplay {
+    constructor(parent, level) {
+        this.dom = elt("div", { class: "game" }, drawGrid(level));
+        this.actorLayer = null;
+        parent.appendChild(this.dom);
+    }
+
+    clear() { this.dom.remove(); }
+}
+
+DOMDisplay.prototype.syncState = function (state) {
+    if (this.actorLayer) this.actorLayer.remove();
+    this.actorLayer = drawActors(state.actors);
+    this.dom.appendChild(this.actorLayer);
+    this.dom.className = `game ${state.status}`;
+    this.scrollPlayerIntoView(state);
+};
+
+DOMDisplay.prototype.scrollPlayerIntoView = function (state) {
+    let width = this.dom.clientWidth;
+    let height = this.dom.clientHeight;
+    let margin = width / 3;
+
+    // The viewport
+    let left = this.dom.scrollLeft, right = left + width;
+    let top = this.dom.scrollTop, bottom = top + height;
+
+    let player = state.player;
+    let center = player.pos.plus(player.size.times(0.5))
+        .times(scale);
+
+    if (center.x < left + margin) {
+        this.dom.scrollLeft = center.x - margin;
+    } else if (center.x > right - margin) {
+        this.dom.scrollLeft = center.x + margin - width;
+    }
+    if (center.y < top + margin) {
+        this.dom.scrollTop = center.y - margin;
+    } else if (center.y > bottom - margin) {
+        this.dom.scrollTop = center.y + margin - height;
+    }
+};
+
+const scale = 64;
+
+function drawGrid(level) {
+    return elt("table", {
+        class: "background",
+        style: `width: ${level.width * scale}px`
+    }, ...level.rows.map(row =>
+        elt("tr", { style: `height: ${scale}px` },
+            ...row.map(type => elt("td", { class: type })))
+    ));
+}
+
+function drawActors(actors) {
+    return elt("div", {}, ...actors.map(actor => {
+        let rect = elt("div", { class: `actor ${actor.type}` });
+        rect.style.width = `${actor.size.x * scale}px`;
+        rect.style.height = `${actor.size.y * scale}px`;
+        rect.style.left = `${actor.pos.x * scale}px`;
+        rect.style.top = `${actor.pos.y * scale}px`;
+        return rect;
+    }));
+}
+
+Level.prototype.touches = function (pos, size, type) {
+    var xStart = Math.floor(pos.x);
+    var xEnd = Math.ceil(pos.x + size.x);
+    var yStart = Math.floor(pos.y);
+    var yEnd = Math.ceil(pos.y + size.y);
+
+    for (var y = yStart; y < yEnd; y++) {
+        for (var x = xStart; x < xEnd; x++) {
+            let isOutside = x < 0 || x >= this.width ||
+                y < 0 || y >= this.height;
+            let here = isOutside ? "wall" : this.rows[y][x];
+            if (here == type) return true;
+        }
+    }
+    return false;
+};
+
+
+State.prototype.update = function (time, keys) {
+    let actors = this.actors
+        .map(actor => actor.update(time, this, keys));
+    let newState = new State(this.level, actors, this.status);
+
+    if (newState.status != "playing") return newState;
+
+    let player = newState.player;
+    if (this.level.touches(player.pos, player.size, "lava")) {
+        return new State(this.level, actors, "lost");
+    }
+
+    for (let actor of actors) {
+        if (actor != player && overlap(actor, player)) {
+            newState = actor.collide(newState);
+        }
+    }
+    return newState;
+};
+
+function overlap(actor1, actor2) {
+    return actor1.pos.x + actor1.size.x > actor2.pos.x &&
+        actor1.pos.x < actor2.pos.x + actor2.size.x &&
+        actor1.pos.y + actor1.size.y > actor2.pos.y &&
+        actor1.pos.y < actor2.pos.y + actor2.size.y;
+}
+
+const playerXSpeed = 7;
+const gravity = 30;
+const jumpSpeed = 10;
+
+Player.prototype.update = function (time, state, keys) {
+    let xSpeed = 0;
+    if (keys.a) xSpeed -= playerXSpeed;
+    if (keys.d) xSpeed += playerXSpeed;
+    let pos = this.pos;
+    let movedX = pos.plus(new Vec(xSpeed * time, 0));
+    if (!state.level.touches(movedX, this.size, "wall")) {
+        pos = movedX;
+    }
+
+    let ySpeed = this.speed.y + time * gravity;
+    let movedY = pos.plus(new Vec(0, ySpeed * time));
+    if (!state.level.touches(movedY, this.size, "wall")) {
+        pos = movedY;
+    } else if (keys.w && ySpeed > 0) {
+        ySpeed = -jumpSpeed;
+    } else {
+        ySpeed = 0;
+    }
+    return new Player(pos, new Vec(xSpeed, ySpeed));
+};
+
+function trackKeys(keys) {
+    let down = Object.create(null);
+    function track(event) {
+        if (keys.includes(event.key)) {
+            down[event.key] = event.type == "keydown";
+        }
+    }
+    window.addEventListener("keydown", track);
+    window.addEventListener("keyup", track);
+    return down;
+}
+
+const arrowKeys =
+    trackKeys(["a", "d", "w"]);
+
+function runAnimation(frameFunc) {
+    let lastTime = null;
+    function frame(time) {
+        if (lastTime != null) {
+            let timeStep = Math.min(time - lastTime, 100) / 1000;
+            if (frameFunc(timeStep) === false) return;
+        }
+        lastTime = time;
+        requestAnimationFrame(frame);
+    }
+    requestAnimationFrame(frame);
+}
+
+function runLevel(level, Display) {
+    let display = new Display(document.body, level);
+    let state = State.start(level);
+    let ending = 1;
+    return new Promise(resolve => {
+        runAnimation(time => {
+            state = state.update(time, arrowKeys);
+            display.syncState(state);
+            if (state.status == "playing") {
+                return true;
+            } else if (ending > 0) {
+                ending -= time;
+                return true;
+            } else {
+                display.clear();
+                resolve(state.status);
+                return false;
+            }
+        });
+    });
+}
+
+async function runGame(plans, Display) {
+    for (let level = 0; level < plans.length;) {
+        let status = await runLevel(new Level(plans[level]),
+            Display);
+        if (status == "won") level++;
+    }
+    console.log("You've won!");
+}
+
+
+class CanvasDisplay {
+    constructor(level) {
+        this.canvas = document.getElementById('game-canvas');
+        this.canvas.width = 950;
+        this.canvas.height = 550;
+
+        this.cx = this.canvas.getContext("2d");
+
+        this.flipPlayer = false;
+
+        this.viewport = {
+            left: 0,
+            top: 0,
+            width: this.canvas.width / scale,
+            height: this.canvas.height / scale
+        };
+    }
+
+    clear() {
+        this.canvas.remove();
+    }
+}
+
+CanvasDisplay.prototype.syncState = function (state) {
+    this.updateViewport(state);
+    this.clearDisplay(state.status);
+    this.drawBackground(state.level);
+    this.drawActors(state.actors);
+};
+
+
+CanvasDisplay.prototype.updateViewport = function (state) {
+    let view = this.viewport, margin = view.width / 3;
+    let player = state.player;
+    let center = player.pos.plus(player.size.times(0.5));
+
+    if (center.x < view.left + margin) {
+        view.left = Math.max(center.x - margin, 0);
+    } else if (center.x > view.left + view.width - margin) {
+        view.left = Math.min(center.x + margin - view.width,
+            state.level.width - view.width);
+    }
+    if (center.y < view.top + margin) {
+        view.top = Math.max(center.y - margin, 0);
+    } else if (center.y > view.top + view.height - margin) {
+        view.top = Math.min(center.y + margin - view.height,
+            state.level.height - view.height);
+    }
+};
+
+CanvasDisplay.prototype.clearDisplay = function (status) {
+    if (status == "won") {
+        this.cx.fillStyle = "rgb(68, 191, 255)";
+    } else if (status == "lost") {
+        this.cx.fillStyle = "rgb(44, 136, 214)";
+    } else {
+        this.cx.fillStyle = "rgb(52, 166, 251)";
+    }
+    this.cx.fillRect(0, 0,
+        this.canvas.width, this.canvas.height);
+};
+
+let otherSprites = document.createElement("img");
+otherSprites.src = "background_sprites.png";
+
+CanvasDisplay.prototype.drawBackground = function (level) {
+    let { left, top, width, height } = this.viewport;
+    let xStart = Math.floor(left);
+    let xEnd = Math.ceil(left + width);
+    let yStart = Math.floor(top);
+    let yEnd = Math.ceil(top + height);
+
+    for (let y = yStart; y < yEnd; y++) {
+        for (let x = xStart; x < xEnd; x++) {
+            let tile = level.rows[y][x];
+            if (tile == "empty") continue;
+            let screenX = (x - left) * scale;
+            let screenY = (y - top) * scale;
+            let tileX = tile == "lava" ? scale : 0;
+            this.cx.drawImage(otherSprites,
+                tileX, 0, scale, scale,
+                screenX, screenY, scale, scale);
+        }
+    }
+};
+
+function flipHorizontally(context, around) {
+    context.translate(around, 0);
+    context.scale(-1, 1);
+    context.translate(-around, 0);
+}
+
+let playerSprites = document.createElement("img");
+playerSprites.src = "spritesheet2.png";
+const playerXOverlap = 38;
+const playerYOverlap = 10;
+
+CanvasDisplay.prototype.drawPlayer = function (player, x, y,
+    width, height) {
+    width += playerXOverlap * 2;
+    height += playerYOverlap
+    x -= playerXOverlap;
+    if (player.speed.x != 0) {
+        this.flipPlayer = player.speed.x < 0;
+    }
+
+    let tile = 8;
+    if (player.speed.y != 0) {
+        tile = 0;
+    } else if (player.speed.x != 0) {
+        tile = Math.floor(Date.now() / 60) % 11;
+    }
+
+    this.cx.save();
+    if (this.flipPlayer) {
+        flipHorizontally(this.cx, x + width / 2);
+    }
+    let tileX = tile * width;
+    this.cx.drawImage(playerSprites, tileX, 0, width, height,
+        x, y, width, height);
+    this.cx.restore();
+};
+
+
+CanvasDisplay.prototype.drawActors = function (actors) {
+    for (let actor of actors) {
+        let width = actor.size.x * scale;
+        let height = actor.size.y * scale;
+        let x = (actor.pos.x - this.viewport.left) * scale;
+        let y = (actor.pos.y - this.viewport.top) * scale;
+        if (actor.type == "player") {
+            this.drawPlayer(actor, x, y, width, height);
+        }
+        // else {
+        //     let tileX = (actor.type == "coin" ? 2 : 1) * scale;
+        //     this.cx.drawImage(otherSprites,
+        //         tileX, 0, width, height,
+        //         x, y, width, height);
+        // }
+    }
+};
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    const SCALE = .5;
-    const WIDTH = 223;
-    const HEIGHT = 225;
-    const SCALED_WIDTH = SCALE * WIDTH;
-    const SCALED_HEIGHT = SCALE * HEIGHT;
-    const CYCLE_LOOP = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-    const FACING_LEFT = 0;
-    const FACING_RIGHT = 1;
 
-    const GOING_UP = 1;
-    const GOING_DOWN = 1;
+    let GAME_LEVELS = [
+        `..................../..................../..................../................####/@.......######....../#####............###/#####++++++++++++###/#####++++++++++++###/####################/####################/####################`,
+        `..................../..................../..................../................####/@.......######....../#####............###/#####++++++++++++###/#####++++++++++++###/####################/####################/####################`,
+        `..................../..................../..................../................####/@.......######....../#####............###/#####++++++++++++###/#####++++++++++++###/####################/####################/####################`
+    ];
 
-    const SLASH_RIGHT = 3;
-    const SLASH_LEFT = 2;
-
-    const MOVEMENT_SPEED = 1;
-    const FRAME_LIMIT = 12;
-
-
-    let canvasEl = document.getElementById('game-canvas');
-    let ctx = canvasEl.getContext('2d');
-    let keyPresses = {};
-    let currentDirection = FACING_RIGHT;
-    let lastDirection = "RIGHT"
-    let currentLoopIndex = 0;
-    let frameCount = 0;
-    let positionX = 0;
-    let positionY = 300;
-    let img = new Image();
-
-
-    window.addEventListener('keydown', keyDownListener, false);
-    function keyDownListener(event) {
-        keyPresses[event.key] = true;
-    }
-
-    window.addEventListener('keyup', keyUpListener, false);
-    function keyUpListener(event) {
-        keyPresses[event.key] = false;
-    }
-
-
-    function loadImage() {
-        img.src = './spritesheet.png'
-        img.onload = function () {
-            window.requestAnimationFrame(gameLoop);
-        }
-    }
-
-    function drawFrame(frameX, frameY, canvasX, canvasY) {
-        ctx.drawImage(img,
-            frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT,
-            canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
-    }
-
-    loadImage();
-
-    function gameLoop() {
-
-        ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-
-        let hasMoved = false;
-
-        if (keyPresses.w && lastDirection === "RIGHT") {
-            moveCharacter(0, -MOVEMENT_SPEED, FACING_RIGHT);
-            hasMoved = true;
-        } else if (keyPresses.s && lastDirection === "RIGHT") {
-            moveCharacter(0, MOVEMENT_SPEED, FACING_RIGHT);
-            hasMoved = true;
-        }
-        if (keyPresses.w && lastDirection === "LEFT") {
-            moveCharacter(0, -MOVEMENT_SPEED, FACING_LEFT);
-            hasMoved = true;
-        } else if (keyPresses.s && lastDirection === "LEFT") {
-            moveCharacter(0, MOVEMENT_SPEED, FACING_LEFT);
-            hasMoved = true;
-        }
-        if (keyPresses.a) {
-            moveCharacter(-MOVEMENT_SPEED, 0, FACING_LEFT);
-            hasMoved = true;
-            lastDirection = "LEFT"
-        } else if (keyPresses.d) {
-            moveCharacter(MOVEMENT_SPEED, 0, FACING_RIGHT);
-            hasMoved = true;
-            lastDirection = "RIGHT"
-        }
-
-        if (keyPresses.Shift && lastDirection === "RIGHT") {
-            moveCharacter(0, 0, SLASH_RIGHT)
-            hasMoved = true;
-        } else if (keyPresses.Shift && lastDirection === "LEFT") {
-            moveCharacter(0, 0, SLASH_LEFT)
-            hasMoved = true;
-        }
-
-        if (hasMoved) {
-            frameCount++;
-            if (frameCount >= FRAME_LIMIT) {
-                frameCount = 0;
-                currentLoopIndex++;
-                if (currentLoopIndex >= CYCLE_LOOP.length) {
-                    currentLoopIndex = 0;
-                }
-            }
-        }
-
-        if (!hasMoved) {
-            currentLoopIndex = 0;
-        }
-
-        drawFrame(CYCLE_LOOP[currentLoopIndex], currentDirection, positionX, positionY);
-        window.requestAnimationFrame(gameLoop);
-    }
-
-    function moveCharacter(deltaX, deltaY, direction) {
-        if ((positionX + deltaX > 0) && (positionX + SCALED_WIDTH + deltaX < canvasEl.width)) {
-            positionX += deltaX;
-        }
-        if ((positionY + deltaY > 0) && (positionY + SCALED_HEIGHT + deltaY < canvasEl.height)) {
-            positionY += deltaY;
-        }
-        currentDirection = direction;
-    }
+    runGame(GAME_LEVELS, CanvasDisplay);
 
 });
-
